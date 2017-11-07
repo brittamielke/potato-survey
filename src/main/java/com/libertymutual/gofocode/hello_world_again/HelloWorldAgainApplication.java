13e8f012c639d6a806f3153aa615447f6d3e6c5d
Britta Mielke <britta.mielke@libertymutual.com>
2017-11-07 14:09:10 -0500

    Initial commit of the potato survey application

---
+++ src/main/java/com/libertymutual/gofocode/hello_world_again/HelloWorldAgainApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.gofocode.hello_world_again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAgainApplication.class, args);
+	}
+}
